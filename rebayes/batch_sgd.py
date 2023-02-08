6e0cc83e1bb8834b14083635508a297a8970bd62
Gerardo Duran-Martin <g.duran@me.com>
2023-02-08 11:25:37 +0000

    feat: batch_sgd
    Define ReFifSGD

--- rebayes/batch_sgd.py
+++ rebayes/batch_sgd.py
@@ -168,3 +168,82 @@ def train_fifo(
     steps = jnp.arange(num_obs)
     state, losses = jax.lax.scan(epoch_step, state, steps)
     return state, losses
+
+
+class ReFifoSGD:
+    """
+    Recursive FIFO SGD.
+    """
+    def __init__(self, buffer_size, lossfn, n_inner=1):
+        self.buffer_size = buffer_size
+        self.lossfn = lossfn
+        self.loss_grad = jax.value_and_grad(self.lossfn, 0)
+        self.n_inner = n_inner
+    
+
+    def predict_obs(self, bel, X):
+        yhat = bel.apply_fn(bel.params, X)
+        return yhat
+
+    def predict_state(self, bel, X):
+        return bel
+    
+    @partial(jax.jit, static_argnums=(0,))
+    def _get_fifo_batches(self, ix):
+        ix_lookback = (ix - self.buffer_size) + 1
+        batches = jnp.arange(self.buffer_size) + ix_lookback
+        return batches
+
+    
+    @partial(jax.jit, static_argnums=(0,))
+    def _train_step(
+        self,
+        X: Float[Array, "num_obs dim_obs"],
+        y: Float[Array, "num_obs dim_obs"],
+        ixs: Int[Array, "batch_len"],
+        state: TrainState,
+    ) -> Tuple[float, TrainState]:
+        """
+        """
+        loss, grads = self.loss_grad(state.params, X, y, ixs, state.apply_fn)
+        state = state.apply_gradients(grads=grads)
+        return loss, state
+
+    @partial(jax.jit, static_argnums=(0,))
+    def update_state(self, bel, X, y, ixs):
+        def partial_step(_, bel):
+            loss_train, bel = self._train_step(X, y, ixs, bel)
+            return bel
+        bel = jax.lax.fori_loop(0, self.n_inner - 1, partial_step, bel)
+        loss, bel = self._train_step(X, y, ixs, bel)
+        return loss, bel
+    
+    def scan(
+        self,
+        state: TrainState,
+        X: Float[Array, "num_obs dim_obs"],
+        y: Float[Array, "num_obs"],
+        X_test: Union[None, Float[Array, "num_obs_test dim_obs"]] = None,
+        y_test: Union[None, Float[Array, "num_obs_test"]] = None,
+        loss_test_fn: Callable = None,
+    ):
+        num_obs = X.shape[0]
+
+        def epoch_step(state, t):
+            ixs = self._get_fifo_batches(t)
+            loss_train, state = self.update_state(state, X, y, ixs)
+
+            if (X_test is not None) and (y_test is not None):
+                loss_test = loss_test_fn(state.params, X_test, y_test, state.apply_fn)
+            else:
+                loss_test = None
+
+            losses = {
+                "train": loss_train,
+                "test": loss_test,
+            } 
+            return state, losses
+        
+        steps = jnp.arange(num_obs)
+        state, losses = jax.lax.scan(epoch_step, state, steps)
+        return state, losses
