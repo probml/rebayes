51c220613e4526b98e2eb4ea225415e412800fed
Gerardo Duran-Martin <g.duran@me.com>
2023-02-10 16:09:38 +0000

    feat: variatonal_continual_learning

--- rebayes/variational_continual_learning.py
+++ rebayes/variational_continual_learning.py
@@ -10,14 +10,32 @@ buffer:
     with the new datapoint.
 2. Random buffer: we replace a random datapoint in the buffer
     with the new datapoint.
+
+
+[2] Farquhar, S., Osborne, M., & Gal, Y. (2019).
+    Radial Bayesian Neural Networks: Beyond Discrete Support
+    In Large-Scale Bayesian Deep Learning. doi:10.48550/ARXIV.1907.00865
 """
 import jax
 import jax.numpy as jnp
+import flax.linen as nn
 from typing import Tuple
 from functools import partial
 from rebayes.base import Rebayes
+from jax.flatten_util import ravel_pytree
 from jaxtyping import Float, Array, Int
 from flax.training.train_state import TrainState
+
+BatchDense = nn.vmap(
+    nn.Dense,
+    in_axes=0, out_axes=0,
+    variable_axes={"params": 0},
+    split_rngs={"params": True},
+)
+
+def get_leaves(params):
+    flat_params, _ = ravel_pytree(params)
+    return flat_params
 
 class VCLState(TrainState):
     """
@@ -25,12 +43,16 @@ class VCLState(TrainState):
     we update the coreset (buffer) with probability
     and replace the oldest datapoint in the buffer
     if its full.
+
+    We take the MFVI assumption over the parameters
     """
     buffer_size: int
     num_obs: int
     buffer_X: Float[Array, "buffer_size dim_features"]
     buffer_y: Float[Array, "buffer_size dim_output"]
     counter: Int[Array, "buffer_size"]
+    prior_mean: Float[Array, "dim_params"]
+    prior_std: Float[Array, "dim_params"]
     p_replace: float = 1.0
 
     def _update_buffer(self, step, buffer, item):
@@ -38,6 +60,33 @@ class VCLState(TrainState):
         buffer = buffer.at[ix_buffer].set(item)
         return buffer
     
+    @staticmethod
+    def _transform(eps, params):
+        mean, rho = params
+        std = jnp.log(1 + jnp.exp(rho))
+        weight = mean + std * eps
+        return weight
+    
+    def _sample_rbnn_params(self, scale=1.0):
+        """
+        Sample from a radial Bayesian neural network
+        radial BNN of [2]. We modify the definition of the
+        RBNN to include a scale parameter, which allows us
+        to control the prior uncertainty over the posterior predictive.
+        """
+        key = jax.random.fold_in(self.key, self.step)
+        key_eps, key_rho = jax.random.split(key)
+        num_params = len(get_leaves(state.mean))
+
+        # The radial dimension.
+        r = jax.random.normal(key_rho) * scale
+
+        eps = jax.random.normal(key_eps, (num_params,))
+        eps = eps / jnp.linalg.norm(eps) * r
+        eps = self.reconstruct_fn(eps)
+
+        params = jax.tree_map(self._transform, eps, self.params)
+        return params
     
     def apply_buffers(self, X, y):
         n_count = self.num_obs
@@ -55,7 +104,8 @@ class VCLState(TrainState):
         
     @classmethod
     def create(cls, *, apply_fn, params, tx,
-               buffer_size, dim_features, dim_output, **kwargs):
+               buffer_size, dim_features, dim_output,
+               prior_mean, prior_std, **kwargs):
         """
         Here, params refers to the parameters of the
         variational distribution for non-coreset datapoint.
@@ -76,6 +126,8 @@ class VCLState(TrainState):
             buffer_X=buffer_X,
             buffer_y=buffer_y,
             counter=counter,
+            prior_mean=prior_mean,
+            prior_std=prior_std,
             **kwargs
         )
 
@@ -105,7 +157,11 @@ class CoresetVCL(Rebayes):
         by (3) in Algorithm 1 of the paper.
         """
         raise NotImplementedError
-    
+        X, y = bel.buffer_X, bel.buffer_y
+        bel = self._update_state_true(bel, X, y)
+        return bel
+
+
     @partial(jax.jit, static_argnums=(0,))
     def _train_step(
         self,
@@ -145,6 +201,6 @@ class CoresetVCL(Rebayes):
         accept = jax.random.bernoulli(keyt, bel.p_replace)
         operands = (bel, Xt, yt)
         bel = jax.lax.cond(
-            accept,self._update_state_true, self._update_state_false, operands
+            accept, self._update_state_true, self._update_state_false, operands
         )
         return bel
