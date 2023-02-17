95b721ceeefb7e599d87e022d777f7b1042b0606
Gerardo Duran-Martin <g.duran@me.com>
2023-02-17 10:36:49 +0000

    refactor (in-progress): l-rvga
    Add notebook and script

--- rebayes/gauss_approx.py
+++ rebayes/gauss_approx.py
@@ -17,7 +17,7 @@ from functools import partial
 from dataclasses import dataclass
 from jaxtyping import Array, Float
 from jax.flatten_util import ravel_pytree
-
+from rebayes.base import Rebayes
 from flax import struct
 
 
@@ -27,11 +27,18 @@ class LRVGAState:
     W: Float[Array, "dim_params dim_subspace"]
     Psi: Float[Array, "dim_params"]
 
+
+# Homoskedastic case (we estimate sigma at a warmup stage)
 @struct.dataclass
 class FlaxLRVGAState:
     mu: Float[Array, "dim_params"]
     W: Float[Array, "dim_params dim_subspace"]
     Psi: Float[Array, "dim_params"]
+    sigma: float = 1.0
+
+    @property
+    def mean(self):
+        return self.mu
 
 
 @dataclass
@@ -313,3 +320,182 @@ def lrvga(
     obs = (keys, X, y)
     state_final, mu_hist = jax.lax.scan(run_lrvga, state_init, obs)
     return state_final, mu_hist
+
+
+class LRVGA(Rebayes):
+    """
+    Limited-memory recursive variational Gaussian approximation (LRVGA)
+    for a homoskedastic Gaussian model with known mean
+    """
+    def __init__(
+            self,
+            # rebayes_params: FlaxLRVGAState # bel
+            fwd_link: Callable,
+            log_prob: Callable,
+            alpha: float = 1.0,
+            beta: float = 1.0,
+            n_inner: int = 3,
+            n_inner_fa: int = 3,
+            n_samples: int = 6
+    ):
+        self.fwd_link = fwd_link
+        self.log_prob = log_prob
+        self.alpha = alpha
+        self.beta = beta
+        self.n_inner = n_inner
+        self.n_inner_fa = n_inner_fa
+        self.n_samples = n_samples
+        self.grad_log_prob = jax.grad(log_prob, argnums=0)
+
+    @staticmethod
+    def _sample_lr_params(key, bel):
+        """
+        Sample parameters from a low-rank variational Gaussian approximation.
+        This implementation avoids the explicit construction of the
+        (D x D) covariance matrix.
+
+        We take s ~ N(0, W W^T + Psi I)
+
+        Implementation based on §4.2.2 of the L-RVGA paper.
+
+        TODO(?): refactor code into jax.vmap. (It faster?)
+        """
+        key_x, key_eps = jax.random.split(key)
+        dim_full, dim_latent = bel.W.shape
+        Psi_inv = 1 / bel.Psi
+
+        eps_sample = jax.random.normal(key_eps, (dim_latent,))
+        x_sample = jax.random.normal(key_x, (dim_full,)) * jnp.sqrt(Psi_inv)
+
+        I_full = jnp.eye(dim_full)
+        I_latent = jnp.eye(dim_latent)
+        # M = I + W^T Psi^{-1} W
+        M = I_latent + jnp.einsum("ji,j,jk->ik", bel.W, Psi_inv, bel.W)
+        # L = Psi^{-1} W^T M^{-1}
+        L_tr = jnp.linalg.solve(M.T, jnp.einsum("i,ij->ji", Psi_inv, bel.W))
+
+        # samples = (I - LW^T)x + Le
+        term1 = I_full - jnp.einsum("ji,kj->ik", L_tr, bel.W)
+        x_transform = jnp.einsum("ij,j->i", term1, x_sample)
+        eps_transform = jnp.einsum("ji,j->i", L_tr, eps_sample)
+        samples = x_transform + eps_transform
+        return samples + bel.mu
+
+    @staticmethod
+    def _get_coef(params, bel, x, fwd_link):
+        c, std = jax.jacfwd(fwd_link, has_aux=True)(params, bel, x)
+        return c * std
+
+    @partial(jax.vmap, in_axes=(None, 0, None, None, None))
+    def _sample_grad_expected_log_prob(self, key, bel, x, y):
+        """
+        E[∇ logp(y|x,θ)]
+        """
+        mu_sample = self._sample_lr_params(key, bel)
+        grads = self.grad_log_prob(mu_sample, x, y)
+        return grads
+
+    @partial(jax.vmap, in_axes=(None, 0, None, None))
+    def _sample_cov_coeffs(self, key, x, bel):
+        params = self._sample_lr_params(key, bel)
+        coef = self._get_coef(params, bel, x, self.fwd_link)
+        return coef
+
+    def _fa_approx_step(
+        self,
+        x: Float[Array, "dim_params"],
+        bel: LRVGAState,
+        bel_prev: LRVGAState,
+    ) -> LRVGAState:
+        """
+        Factor Analysis (FA) approximation to the low-rank (W)
+        and diagonal (Psi) matrices.
+        """
+        # Load data
+        W_prev, Psi_prev = bel_prev.W, bel_prev.Psi
+        W, Psi = bel.W, bel.Psi
+        
+        # Initialise basic transformations
+        _, dim_latent = W.shape
+        I = jnp.eye(dim_latent)
+        Psi_inv = 1 / Psi
+        
+        # Construct helper matrices
+        M = I + jnp.einsum("ij,i,ik->jk", W, Psi_inv, W)
+        M_inv = jnp.linalg.inv(M)
+        V_beta = jnp.einsum("...i,...j,j,jk->ik", x, x, Psi_inv, W)
+        V_alpha = (
+            jnp.einsum("ij,kj,k,kl->il", W_prev, W_prev, Psi_inv, W) +
+            jnp.einsum("i,i,ij->ij", Psi_prev, Psi_inv, W)
+        )
+        V = self.beta * V_beta + self.alpha * V_alpha
+        # Value_update
+        # (return transpose of W_solve -- avoid extra transpose op)
+        W_solve = I + jnp.einsum("ij,kj,k,kl->li", M_inv, W, Psi_inv, V)
+        W = jnp.linalg.solve(W_solve, V.T).T
+        Psi = (
+            self.beta * jnp.einsum("...i,...i->i", x, x) +
+            self.alpha * jnp.einsum("ij,ij->i", W_prev, W_prev) + 
+            self.alpha * Psi_prev -
+            jnp.einsum("ij,jk,ik->i", W, M_inv, V)
+        )
+        
+        new_bel = bel.replace(
+            mu=bel.mu,
+            W=W,
+            Psi=Psi
+        )
+        return new_bel
+        
+    @partial(jax.jit, static_argnums=(0,))
+    def _sample_half_fisher(self, key, x, bel):
+        """
+        Estimate X such that
+            X X^T ~ E_{q(θ)}[E_{y}[∇^2 log p(y|x,θ)]]
+        """
+        keys = jax.random.split(key, self.n_samples)
+        coeffs = self._sample_cov_coeffs(keys, x, bel) / jnp.sqrt(self.n_samples)
+        # XXtr = jnp.einsum("nji,njk->ik", coeffs, coeffs) / num_samples
+        return coeffs
+
+    def _step_lrvga(self, state, obs):
+        """
+        Iterated RVGA (§4.2.1). We omit the second iteration of the covariance matrix
+        """
+        key, x, y = obs
+        key_fisher, key_est, key_mu_final = jax.random.split(key, 3)
+
+        X = sample_half_fisher(key_fisher, x, state, n_samples, model, reconstruct_fn)
+        def fa_partial(_, new_state):
+            new_state = fa_approx_step(X, new_state, state, alpha, beta)
+            return new_state
+
+        # Algorithm 1 in §3.2 of L-RVGA states that 1 to 3 loops may be enough in
+        # the inner (fa-update) loop
+        state_update = jax.lax.fori_loop(0, n_inner, fa_partial, state)
+        # First mu update
+        mu_add = mu_update(key_est, x, y, state, state_update, n_samples, model, reconstruct_fn)
+        mu_new = state.mu + mu_add
+        state_update = state_update.replace(mu=mu_new)
+        # Second mu update: we use the updated state to estimate the gradient
+        mu_add = mu_update(key_mu_final, x, y, state_update, state_update, n_samples, model, reconstruct_fn)
+        mu_new = state.mu + mu_add
+        state_update = state_update.replace(mu=mu_new)
+        return state_update, state_update.mu
+
+    def init_bel(self):
+        raise NotImplementedError
+
+    def predict_obs(self, bel, X):
+        yhat = self.fwd_link(bel, X)
+        return yhat
+
+    def predict_state(self, bel):
+        """
+        L-RVGA doesn't have a closed-form prediction for the state
+        (check)
+        """
+        return bel
+
+    def update_state(self, bel, Xt, yt):
+        ...
