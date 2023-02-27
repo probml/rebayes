f57d2db5c7dbfb994d65ed4a576107803e18bf68
Gerardo Duran-Martin <g.duran@me.com>
2023-02-27 19:02:01 +0000

    feat: datasets.py
    Define make_rotating_moons

--- rebayes/utils/datasets.py
+++ rebayes/utils/datasets.py
@@ -91,7 +91,6 @@ class LRDataset:
         return X, y
 
 
-
 def showdown_preprocess(
         train, test, n_warmup=1000, n_test_warmup=100, xaxis=0,
         normalise_target=True, normalise_features=True,
@@ -578,3 +577,82 @@ def make_showdown_moons(n_train, n_test, n_train_warmup, n_test_warmup, noise, s
     warmup_test = jax.tree_map(jnp.array, warmup_test)
 
     return train, test, warmup_train, warmup_test
+
+def _rotation_matrix(angle):
+    """
+    Create a rotation matrix that rotates the
+    space 'angle'-radians.
+    """
+    R = np.array([
+        [np.cos(angle), -np.sin(angle)],
+        [np.sin(angle), np.cos(angle)]
+    ])
+    return R
+
+
+def _make_rotating_moons(radians, n_samples=100, **kwargs):
+    """
+    Make two interleaving half circles rotated by 'radians' radians
+    
+    Parameters
+    ----------
+    radians: float
+        Angle of rotation
+    n_samples: int
+        Number of samples
+    **kwargs:
+        Extra arguments passed to the `make_moons` function
+    """
+    X, y = make_moons(n_samples=n_samples, **kwargs)
+    X = jnp.einsum("nm,mk->nk", X, _rotation_matrix(radians))
+    return X, y
+
+
+def make_rotating_moons(n_train, n_test, n_rotations, min_angle=0, max_angle=360, seed=314, **kwargs):
+    """
+    n_train: int
+        Number of training samples per rotation
+    n_test: int
+        Number of test samples per rotation
+    n_rotations: int
+        Number of rotations
+    """
+    np.random.seed(seed)
+    n_samples = n_train + n_test
+    min_rad = np.deg2rad(min_angle)
+    max_rad = np.deg2rad(max_angle)
+
+    radians = np.linspace(min_rad, max_rad, n_rotations)
+    X_train_all, y_train_all, rads_train_all = [], [], []
+    X_test_all, y_test_all, rads_test_all = [], [], []
+    for rad in radians:
+        X, y = _make_rotating_moons(rad, n_samples=n_samples, **kwargs)
+        rads = jnp.ones(n_samples) * rad
+
+        X_train = X[:n_train]
+        y_train = y[:n_train]
+        rad_train = rads[:n_train]
+
+        X_test = X[n_train:]
+        y_test = y[n_train:]
+        rad_test = rads[n_train:]
+
+        X_train_all.append(X_train)
+        y_train_all.append(y_train)
+        rads_train_all.append(rad_train)
+
+        X_test_all.append(X_test)
+        y_test_all.append(y_test)
+        rads_test_all.append(rad_test)
+
+    X_train_all = jnp.concatenate(X_train_all, axis=0)
+    y_train_all = jnp.concatenate(y_train_all, axis=0)
+    rads_train_all = jnp.concatenate(rads_train_all, axis=0)
+    X_test_all = jnp.concatenate(X_test_all, axis=0)
+    y_test_all = jnp.concatenate(y_test_all, axis=0)
+    rads_test_all = jnp.concatenate(rads_test_all, axis=0)
+
+    train = (X_train_all, y_train_all, rads_train_all)
+    test = (X_test_all, y_test_all, rads_test_all)
+    
+    return train, test
