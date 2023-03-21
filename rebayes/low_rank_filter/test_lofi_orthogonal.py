44eaa744cbefa2c1fcf962b37036634bfb83d079
petergchang <peter0414@gmail.com>
2023-03-21 11:04:41 +0000

    feat: implement lofi orthogonal as separate class

---
+++ rebayes/low_rank_filter/test_lofi_orthogonal.py
@@ -0,0 +1,57 @@
+import pytest
+
+from rebayes.base import RebayesParams
+from rebayes.low_rank_filter.lofi import LoFiParams, INFLATION_METHODS
+from rebayes.low_rank_filter.lofi_orthogonal import RebayesLoFiOrthogonal
+from rebayes.low_rank_filter.test_orfit import load_rmnist_data
+from rebayes.utils.utils import get_mlp_flattened_params
+
+
+def setup_lofi_orth(memory_size, steady_state, inflation_type):
+    input_dim, hidden_dims, output_dim = 784, [2, 2], 1
+    model_dims = [input_dim, *hidden_dims, output_dim]
+    _, flat_params, _, apply_fn = get_mlp_flattened_params(model_dims)
+    
+    model_params = RebayesParams(
+        initial_mean = flat_params,
+        initial_covariance = 1e-1,
+        dynamics_weights = 1.0,
+        dynamics_covariance = 1e-1,
+        emission_mean_function = apply_fn,
+        emission_cov_function = None,
+        adaptive_emission_cov = True,
+        dynamics_covariance_inflation_factor = 1e-5,
+    )
+
+    lofi_params = LoFiParams(
+        memory_size = memory_size,
+        steady_state = steady_state,
+        inflation = inflation_type,
+    )
+    
+    lofi_estimator = RebayesLoFiOrthogonal(
+        model_params = model_params,
+        lofi_params = lofi_params,
+    )
+    
+    return lofi_estimator
+
+
+@pytest.mark.parametrize(
+    "memory_size, steady_state, inflation_type",
+    [(10, ss, it) for ss in [True, False] for it in INFLATION_METHODS]
+)
+def test_lofi_orthogonal(memory_size, steady_state, inflation_type):
+    # Load rotated MNIST dataset
+    n_train = 200
+    X_train, y_train = load_rmnist_data(n_train)
+    
+    # Define mean callback function
+    def callback(bel, *args, **kwargs):
+        return bel.mean
+    
+    # Test if run without error
+    lofi_estimator = setup_lofi_orth(memory_size, steady_state, inflation_type)
+    _ = lofi_estimator.scan(X_train, y_train, callback)
+    
+    assert True
