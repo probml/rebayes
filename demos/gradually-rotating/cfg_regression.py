7bf11075baa8cdc209afdcacc5d536aab2623f45
Gerardo Duran-Martin <g.duran@me.com>
2023-04-17 21:14:58 +0000

    create: cfg_regression

---
+++ demos/gradually-rotating/cfg_regression.py
@@ -0,0 +1,18 @@
+import ml_collections
+
+cfg = ml_collections.ConfigDict()
+
+cfg.memory = 10
+cfg.forecast = 10
+
+# LoFi parameters
+cfg.lofi = ml_collections.ConfigDict()
+cfg.lofi.dynamics_weight = 1.0
+cfg.lofi.dynamics_covariance = 0.0
+cfg.lofi.initial_covariance = 0.1
+
+# Replay-buffer SGD parameters
+cfg.rsgd = ml_collections.ConfigDict()
+cfg.rsgd.learning_rate = 5e-4
+
+print(cfg)
