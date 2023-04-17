5503f837d1076086bd0ce2c315bd7850b6a01b1e
Gerardo Duran-Martin <g.duran@me.com>
2023-04-17 21:30:01 +0000

    refactor: cfg_regression.py

--- demos/gradually-rotating/cfg_regression.py
+++ demos/gradually-rotating/cfg_regression.py
@@ -1,18 +1,24 @@
 import ml_collections
 
-cfg = ml_collections.ConfigDict()
+def get_config():
+    cfg = ml_collections.ConfigDict()
 
-cfg.memory = 10
-cfg.forecast = 10
+    cfg.memory = 10
+    cfg.forecast = 10
 
-# LoFi parameters
-cfg.lofi = ml_collections.ConfigDict()
-cfg.lofi.dynamics_weight = 1.0
-cfg.lofi.dynamics_covariance = 0.0
-cfg.lofi.initial_covariance = 0.1
+    # LoFi parameters
+    cfg.lofi = ml_collections.ConfigDict()
+    cfg.lofi.dynamics_weight = 1.0
+    cfg.lofi.dynamics_covariance = 0.0
+    cfg.lofi.initial_covariance = 0.1
 
-# Replay-buffer SGD parameters
-cfg.rsgd = ml_collections.ConfigDict()
-cfg.rsgd.learning_rate = 5e-4
+    # Replay-buffer SGD parameters
+    cfg.rsgd = ml_collections.ConfigDict()
+    cfg.rsgd.learning_rate = 5e-4
+    cfg.rsgd.n_inner = 1
 
-print(cfg)
+    return cfg
+
+if __name__ == "__main__":
+    cfg = get_config()
+    print(cfg)
