24aa5295b4250ce66995382459182c5fd6e1ce9f
Gerardo Duran-Martin <g.duran@me.com>
2023-04-19 15:00:02 +0000

    feat: eval_lofi_hparams.py

--- demos/gradually-rotating/eval_lofi_hparams.py
+++ demos/gradually-rotating/eval_lofi_hparams.py
@@ -4,14 +4,18 @@ rotating MNIST dataset with sinusoidal angle of rotation.
 """
 
 import jax
+import numpy as np
 import flax.linen as nn
 import eval_main as ev
+from copy import deepcopy
+from tqdm.auto import tqdm
+from itertools import product
 from functools import partial
 from cfg_regression import get_config
 
 from rebayes.utils.utils import tree_to_cpu
 
-target_digits = [2, 3]
+target_digits = [2, 3, 4]
 n_classes = len(target_digits)
 num_train = 6_000
 
@@ -31,22 +35,40 @@ emission_cov_fn = partial(ev.emission_cov_function, fn_mean=emission_mean_fn)
 
 _, dim_in = data["dataset"]["train"][0].shape
 
-callback_part = partial(ev.callback,
+callback_lofi = partial(ev.callback,
                         apply_fn=emission_mean_fn,
                         X_test=X_train, y_test=Y_train,
+                        recfn=recfn,
                         )
 
-### Lofi---load and train
-agent = ev.load_lofi_agent(cfg, flat_params, emission_mean_fn, emission_cov_fn)
-callback_lofi = partial(callback_part, recfn=recfn)
-bel, outputs_lofi = agent.scan(X_train, Y_train, progress_bar=True, callback=callback_lofi)
-bel = jax.block_until_ready(bel)
-outputs_lofi = tree_to_cpu(outputs_lofi)
+outputs_all = []
 
-### RSGD---load and train
-callback_rsgd = partial(callback_part, recfn=lambda x: x)
-apply_fn = partial(ev.apply_fn_unflat, model=model)
-agent = ev.load_rsgd_agent(cfg, tree_params, apply_fn, ev.lossfn_fifo, dim_in, n_classes)
-bel, outputs_rsgd = agent.scan(X_train, Y_train, progress_bar=True, callback=callback_rsgd)
-bel = jax.block_until_ready(bel)
-outputs_rsgd = tree_to_cpu(outputs_rsgd)
+# eps = np.array([0, 1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4])
+eps = np.array([0, 1e-6, 1e-5, 1e-4])
+list_dynamics_weights = 1 - eps
+list_dynamics_covariance = eps.copy()
+
+elements = list(product(list_dynamics_weights, list_dynamics_covariance))
+
+for dynamics_weight, dynamics_covariance in tqdm(elements):
+    dynamics_weight = float(dynamics_weight)
+    dynamics_covariance = float(dynamics_covariance)
+    cfg_lofi = deepcopy(cfg)
+
+    cfg_lofi.lofi.dynamics_weight = dynamics_weight
+    cfg_lofi.lofi.dynamics_covariance = dynamics_covariance
+
+    agent = ev.load_lofi_agent(cfg_lofi, flat_params, emission_mean_fn, emission_cov_fn)
+    bel, output = agent.scan(X_train, Y_train, progress_bar=False, callback=callback_lofi)
+    bel = jax.block_until_ready(bel)
+
+    bel = tree_to_cpu(bel)
+    output = tree_to_cpu(output)
+
+    res = {
+        "config": cfg_lofi,
+        "outputs": output,
+        "bel": bel,
+    }
+
+    outputs_all.append(res)
