695c463675582991ff578e46844ba92b1ae68d70
Peter G. Chang <peter0414@gmail.com>
2023-06-19 04:13:44 -0500

    refactor: default evaluation on test window

--- demos/collas/run_regression_experiments.py
+++ demos/collas/run_regression_experiments.py
@@ -115,7 +115,6 @@ def _eval_metric(
     obs_scale: float,
     problem: str,
     nll_method: str,
-    window: bool,
 ) -> dict:
     """Get evaluation metric for classification problem type.
     """
@@ -146,7 +145,7 @@ def _eval_metric(
                             label="log_likelihood"),
             "test": partial(callbacks.cb_reg_sup,
                             ymean=0.0, ystd=1.0, 
-                            only_window_eval=window)
+                            only_window_eval=True)
         }
     
     return result
@@ -273,7 +272,7 @@ def main(cl_args):
     )
     dataset_load_fn = partial(dataset_load_fn, dataset=base_dataset)
     eval_metric = _eval_metric(cl_args.obs_scale, cl_args.problem, 
-                               cl_args.nll_method, cl_args.window)
+                               cl_args.nll_method)
     
     # Initialize model
     if cl_args.model == "cnn":
@@ -380,9 +379,6 @@ if __name__ == "__main__":
     
     # Number of random initializations for evaluation
     parser.add_argument("--n_iter", type=int, default=100)
-    
-    # Evaluate on the window only
-    parser.add_argument("--window", action="store_true")
     
     args = parser.parse_args()
     main(args)
