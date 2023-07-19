84eed70a7a2fe4706192a33a848ed372d6b6e2cd
Gerardo Duran-Martin <g.duran@me.com>
2023-07-19 07:02:50 +0000

    feat: subspace_filter.py

--- rebayes/low_rank_filter/subspace_filter.py
+++ rebayes/low_rank_filter/subspace_filter.py
@@ -1,6 +1,7 @@
 import jax
 import chex
 import jax.numpy as jnp
+import flax.linen as nn
 from rebayes import base
 from typing import Union
 from jaxtyping import Array, Float
@@ -8,7 +9,23 @@ from flax.training.train_state import TrainState
 from jax.flatten_util import ravel_pytree
 
 
-# TODO: Create flax classmodel that outputs parameters and a projection matrix
+class SubspaceModule(nn.Module):
+    dim_subspace: int
+    base_module: nn.Module
+    init_normal: Callable = nn.initializers.normal()
+    init_proj: Callable = nn.initializers.normal()
+
+
+    def setup(self, X_buff, y_buff):
+        # TODO:
+        # Define a projection matrix, a bias vector and a reconstruction function
+        ...
+
+    def __call__(self, x):
+        params = self.projection_matrix @ self.w + self.b
+        params = self.reconstruct_params(params)
+        yhat = self.base_module.apply(params, x)
+        return yhat
 
 class SubspaceBel(TrainState):
     projection_matrix: Float[Array, "dim_full dim_subspace"]
