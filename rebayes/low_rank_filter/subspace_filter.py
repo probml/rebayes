de948222760fefbb974cf716f83bef86be3083c4
Gerardo Duran-Martin <g.duran@me.com>
2023-07-22 15:05:02 +0000

    feat: subspace_filter

--- rebayes/low_rank_filter/subspace_filter.py
+++ rebayes/low_rank_filter/subspace_filter.py
@@ -9,6 +9,13 @@ from flax.training.train_state import TrainState
 from jax.flatten_util import ravel_pytree
 
 
+def get_fparams(cls, X):
+    dummy = cls(None, name="d")
+    dummy_params = dummy.init(jax.random.PRNGKey(0), X)
+    dummy_params, _ = ravel_pytree(dummy_params)
+    return len(dummy_params)
+
+
 def subcify(cls):
     class SubspaceModule(nn.Module):
         dim_subspace: int
@@ -16,24 +23,30 @@ def subcify(cls):
         init_proj: Callable = nn.initializers.normal()
 
 
-        # How to get the dim_subspace and dim_full?
+        def init(self, rngs, X, *args, **kwargs):
+            dim_full = get_fparams(cls, X)
+            self.dim_full = dim_full
+            return self.parent().init(self, rngs, *args, **kwargs)
+
         def setup(self):
             self.subspace = self.param(
                 "subspace",
                 self.init_proj,
-                (dim_subspace,)
+                (self.dim_subspace,)
             )
             self.projection = self.param(
                 "projection",
                 self.init_proj,
-                (dim_full, dim_subspace)
+                (self.dim_full, self.dim_subspace)
             )
 
         @nn.compact
         def __call__(self, x):
             params = self.projection @ self.subspace
             params = rfn(params)
-            return NNet().apply(params, x)
+            return cls().apply(params, x)
+    
+    return SubspaceModule
 
 
 class SubspaceBel(TrainState):
