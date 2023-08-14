fix: bug with resampled gradient lofi

rebayes/low_rank_filter/lofi.py
@@ -537,6 +537,11 @@ class RebayesIteratedLoFi(RebayesLoFiDiagonal):
                          emission_cov_function, emission_dist, adaptive_emission_cov, 
                          dynamics_covariance_inflation_factor, memory_size, steady_state, 
                          inflation, use_svd)
+        # def log_likelihood(params, x, y):
+        #     # Linearized Gaussian observation model
+        #     model_mean = self.emission_mean_function(params, x)
+        #     cov = self.emission_cov_function(params, x)
+        #     linearized_mean = 
         self.log_likelihood = lambda params, x, y: \
             jnp.sum(
                 emission_dist(self.emission_mean_function(params, x),
@@ -665,7 +670,6 @@ class GradientLoFiBel:
     nobs: int
     obs_noise_var: float
 
-
 class RebayesGradientLoFi(RebayesLoFiDiagonal):
     def __init__(
         self,
@@ -682,9 +686,8 @@ class RebayesGradientLoFi(RebayesLoFiDiagonal):
         inflation: str = 'bayesian',
         use_svd: bool = True,
         learning_rate: float = 0.01,
-        method: str = "resample",
+        method: str = "re-sample",
         n_sample: int = 10,
-        output_dim: int = 1,
     ):
         super().__init__(dynamics_weights, dynamics_covariance, emission_mean_function, 
                          emission_cov_function, emission_dist, adaptive_emission_cov, 
@@ -698,7 +701,6 @@ class RebayesGradientLoFi(RebayesLoFiDiagonal):
         self.learning_rate = learning_rate
         self.method = method
         self.n_sample = n_sample
-        self.output_dim = output_dim
         
         # Update function
         if self.method == "re-sample":

rebayes/low_rank_filter/lofi_core.py
@@ -920,11 +920,12 @@ def _lofi_diagonal_gradient_resample_condition_on(
     ys = emission_dist(m_Y(m), Cov_Y(m)).sample(seed=key, sample_shape=(n_sample,))
     # Compute gradients and average
     grad_fn = lambda y: grad(log_likelihood, argnums=0)(m, x, y)
-    gll = jnp.mean(vmap(grad_fn)(ys), axis=0)
+    pseudo_gll = jnp.mean(vmap(grad_fn)(ys), axis=0).reshape(-1, 1)
+    gll = grad_fn(y).reshape(-1, 1)
         
     R_chol = jnp.linalg.cholesky(R)
     A = jnp.linalg.lstsq(R_chol, jnp.eye(C))[0].T
-    W_tilde = jnp.hstack([Lambda * U, gll.reshape(P, -1)])
+    W_tilde = jnp.hstack([Lambda * U, pseudo_gll.reshape(P, -1)])
     
     # Update the U matrix
     u, lamb = _fast_svd(W_tilde)
@@ -935,8 +936,9 @@ def _lofi_diagonal_gradient_resample_condition_on(
     Ups_cond = Ups + jnp.einsum('ij,ij->i', W_extra, W_extra)[:, jnp.newaxis]
     
     G = jnp.linalg.pinv(jnp.eye(W_tilde.shape[1]) + W_tilde.T @ (W_tilde/Ups))
-    K = gll @ A.T/Ups - (W_tilde/Ups @ G) @ ((W_tilde/Ups).T @ gll @ A.T)
-    m_cond = m + K @ (y - yhat)
+    next_term = (W_tilde/Ups @ G) @ ((W_tilde/Ups).T @ gll)
+    K = gll/Ups - (W_tilde/Ups @ G) @ ((W_tilde/Ups).T @ gll)
+    m_cond = m + K.ravel()
     
     return m_cond, U_cond, Lambda_cond, Ups_cond
 